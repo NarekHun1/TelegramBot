a251f74670f33525f21f95e1f3332c3492c7184d
NarekHun1 <110663420+NarekHun1@users.noreply.github.com>
2024-11-17 19:50:45 +0400

    Add files via upload

---
+++ media.js
@@ -0,0 +1,36 @@
+const mediaGroup = [
+    {
+        media: 'https://s.list.am/pp/f/035/26097035.webp',
+        type: 'photo',
+        caption: '**about room!**'
+    },
+    {
+        media: 'https://s.list.am/pp/f/035/26097035.webp',
+        type: 'photo',
+    },
+
+    {
+        media: 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        type: 'video',
+    }
+]
+
+const mediaRoom2 = [  {
+    media: 'https://s.list.am/pp/f/035/26097035.webp',
+    type: 'photo',
+    caption: '**about room!**'
+},
+    {
+        media: 'https://s.list.am/pp/f/035/26097035.webp',
+        type: 'photo',
+    },
+
+    {
+        media: 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        type: 'video',
+    }]
+
+module.exports = {
+    mediaGroup,
+    mediaRoom2
+}
---
+++ mediaTv.js
@@ -0,0 +1,10 @@
+const mediaTvGroup = [
+
+    {
+        media: 'https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4',
+        type: 'video',
+    }
+
+]
+
+module.exports = mediaTvGroup
